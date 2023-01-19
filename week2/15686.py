126c7c942879f02e478c89c8ce948591effca9a0
khj1030 <khj011030@gmail.com>
2023-01-19 13:33:10 +0900

    15686 update

---
+++ week2/15686.py
@@ -0,0 +1,33 @@
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+city = []
+for _ in range(n):
+  city.append(list(map(int, input().split())))
+
+anser = 999
+home = []
+c_home = []
+
+for i in range(n):
+    for j in range(n):
+        if city[i][j] == 1:
+            home.append([i, j])
+        elif city[i][j] == 2:
+            c_home.append([i, j])
+
+for c in combinations(c_home, m):
+    temp = 0
+    for h in home: 
+        dist = 999
+        for c_1 in c:
+            dist = min(dist, abs(h[0] - c_1[0]) + abs(h[1] - c_1[1]))
+        temp += dist
+    anser = min(anser, temp)
+
+print(anser)
+
+# 런타임에러...
